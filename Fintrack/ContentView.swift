f19483c0484e4daa51602fa618bcf85aea0f89a7
Adam <112819571+AdamElt@users.noreply.github.com>
2025-03-29 21:57:04 -0400

    Initial Commit

---
+++ Fintrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fintrack
+//
+//  Created by Adam El-Telbani on 3/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fintrack/FintrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FintrackApp.swift
+//  Fintrack
+//
+//  Created by Adam El-Telbani on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FintrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
